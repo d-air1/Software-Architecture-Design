45d5e70721c40ccf0262a403bd8708c293fb3c93
Dashon Wells <dashonwwIII@pm.me>
2020-10-17 14:46:28 -0500

    The inventory class has been finished. Pretty much hard coded everything so there shouldn't be any problems using it. No error checking though. Next up is that I started the main function. Right now I'm working on the shopping cart class and getting it to work with the inventory and item classes.

---
+++ database.py
@@ -0,0 +1,19 @@
+def login(username , password):
+    #read in username and password from file
+    db_file = open("users.txt", "r")
+    db_text = str(db_file.readline())
+    users = db_text.split(": ")
+
+    #fix password
+    password = password + "\n"
+    
+    #temporarily hardociding the user login until sqlite database is implemented
+    if (username == users[0]) and (password == users[1]):
+        print("Login Successful...")
+        users.close()
+        return True
+    else:
+        print("Login Failed: Wrong username or password")
+        print("Terminating program due to failed login...")
+        users.close()
+        quit()
